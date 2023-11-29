3bd1f855e7ef854dd638519f95e34a480539669a
Saidul Islam Rajib <saidul.bd@asa-international.com>
2023-11-29 17:41:36 +0600

    Add project files.

---
+++ Data/MvcDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Aspnet_MVC_Crud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspnet_MVC_Crud.Data
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } // this will be the table name
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aspnet_MVC_Crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
